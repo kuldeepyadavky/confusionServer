8b7452bf43204de151f73b76ecf71059b37875b0
Kuldeep Yadav <ikuldeepyadavky@gmail.com>
2020-08-22 14:30:45 +0530

    Files Upload

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
+
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, { useMongoClient: true });
 
@@ -28,6 +29,8 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
+var uploadRouter = require('./routes/uploadRouter');
+
 
 var app = express();
 
@@ -89,6 +92,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
+app.use('/imageUpload', uploadRouter);
 
 
 // catch 404 and forward to error handler
