2dfe2211301b3814ef4822b9581982a32a3852c6
Kuldeep Yadav <ikuldeepyadavky@gmail.com>
2020-08-27 11:08:17 +0530

    Assignment 4

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
 var uploadRouter = require('./routes/uploadRouter');
 
+var favoriteRouter = require('./routes/favoriteRouter');
+
 
 var app = express();
 
@@ -93,6 +95,7 @@ app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 
 // catch 404 and forward to error handler
